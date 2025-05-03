228daf5ec5b4da49920e3652e1cf1193b7c25629
BaltzarL <106113033+BaltzarL@users.noreply.github.com>
2025-05-03 12:49:58 +0000

    improve core code

--- packages/nest/src/index.ts
+++ packages/nest/src/index.ts
@@ -2,101 +2,61 @@
 import {detectSQLInjection} from "@middleware/core";
 import {Injectable, NestMiddleware} from '@nestjs/common';
 import {Request, Response, NextFunction} from 'express';
+import {
+    ExampleRequestLogger,
+    RequestConverter,
+    RequestHandlerBuilder,
+    RequestWrapper
+} from "@middleware/core/dist/types";
 
-//
-// class NestRequestWrapper extends RequestWrapper<Request> {
-//     constructor(request: Request) {
-//         super(request);
-//     }
-//
-//     set method(newMethod: string) {
-//         this.request.method = newMethod;
-//     }
-//
-//     get method(): string {
-//         return this.request.method;
-//     }
-//
-//     set url(newUrl: string) {
-//         this.request.url = newUrl;
-//     }
-//
-//     get url(): string {
-//         return this.request.url;
-//     }
-// }
-//
-// class NestRequestConverter extends RequestConverter<Request> {
-//     getWrapper(request: Request): RequestWrapper<Request> {
-//         return new NestRequestWrapper(request);
-//     }
-// }
-//
-// // Example request handler
-// class RequestLogger<T> extends RequestHandler<T> {
-//     handleRequest(wrapper: RequestWrapper<T>): RequestWrapper<T> {
-//         console.log(`Request Method: ${wrapper.method}`);
-//         console.log(`Request URL: ${wrapper.url}`);
-//         return wrapper;
-//     }
-// }
-//
-//
-//
-// // // Example request convertor
-// // class ExpressRequestConverter extends RequestConverter<Partial<Request>> {
-// //     getContext(req: Request): RequestContext {
-// //         return {
-// //             method: req.method,
-// //             url: req.url,
-// //             headers: {},
-// //             extend(metadata: RequestMetadata): ExtendedRequest {
-// //                 return new ExtendedRequest(this, metadata);
-// //             }
-// //         };
-// //     }
-// //
-// //     getRequest(ctx: RequestContext): Partial<Request> {
-// //         return {
-// //             method: ctx.method,
-// //             url: ctx.url,
-// //             originalUrl: ctx.url,
-// //             headers: ctx.headers,
-// //             body: ctx.body,
-// //             params: ctx.params || {},
-// //             query: ctx.query || {},
-// //         }
-// //     }
-// // }
-//
-// @Injectable()
-// export class SQLInjectionMiddleware implements NestMiddleware {
-//     converter = new NestRequestConverter();
-//     builder = new RequestHandlerBuilder(converter);
-//     logger = new RequestLogger(converter);
-//
-//     use(req: Request, res: Response, next: NextFunction) {
-//         console.log("RUNNING SQL INJECTION MIDDLEWARE")
-//
-//         this.builder
-//             .intercept(req)
-//             .then(logger)
-//
-//
-//         // if (detectSQLInjection(context)) {
-//         //     return res.status(400).send('Potential SQL Injection detected');
-//         // }
-//
-//         next();
-//     }
-// }
 
+class NestRequestWrapper extends RequestWrapper<Request> {
+    constructor(request: Request) {
+        super(request);
+    }
+
+    set method(newMethod: string) {
+        this.request.method = newMethod;
+    }
+
+    get method(): string {
+        return this.request.method;
+    }
+
+    set url(newUrl: string) {
+        this.request.url = newUrl;
+    }
+
+    get url(): string {
+        return this.request.url;
+    }
+}
+
+class NestRequestConverter extends RequestConverter<Request> {
+    getWrapper(request: Request): RequestWrapper<Request> {
+        return new NestRequestWrapper(request);
+    }
+}
 
 @Injectable()
 export class SQLInjectionMiddleware implements NestMiddleware {
+    converter = new NestRequestConverter();
+    handler = new RequestHandlerBuilder(this.converter);
+    logger = new ExampleRequestLogger();
+
     use(req: Request, res: Response, next: NextFunction) {
+        console.log("RUNNING SQL INJECTION MIDDLEWARE")
         console.log("BANANINI")
         detectSQLInjection("CHIMPANZINI");
+
+        this.handler
+            .intercept(req)
+            .then(this.logger)
+
+        // if (detectSQLInjection(context)) {
+        //     return res.status(400).send('Potential SQL Injection detected');
+        // }
+
         next();
     }
 }
